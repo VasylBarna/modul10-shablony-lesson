77d79d0dfba5bd9d20f0cabe323fe2881d2aca72
VasylBarna <vasylbarna@gmail.com>
2021-05-13 15:48:35 +0300

    comlead

---
+++ src/task1/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Create user presentation list
+ */
+
+import './styles.css';
+
+import template from './userList.hbs';
+
+const data = {
+  title: 'My User List',
+  data: {
+    users: [
+      { name: 'Bobby', age: 15, isAdult: false },
+      { name: 'Freddy', age: 25, isAdult: true },
+      { name: 'Jonny', age: 60, isAdult: true },
+    ],
+  },
+};
+
+const root = document.querySelector('#root');
+root.insertAdjacentHTML('beforeend', template(data));
